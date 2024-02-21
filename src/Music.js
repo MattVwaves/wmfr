0ee95cd2ebbde9c14a35ffc497e00200b5d94e65
Matt Holt <jamesmattholt@gmail.com>
2024-02-21 12:49:40 +0000

    track data added, tracklist displayed dynamically

--- src/Music.js
+++ src/Music.js
@@ -2,8 +2,27 @@ import './App.css';
 import Header from './Header';
 import ReactAudioPlayer from 'react-audio-player';
 import Epiano from './electric_piano.mp3';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import MusicCategories from './MusicTracks';
 
 export default function Music() {
+  const [category, setCategory] = useState('Melodically Rich');
+  const [trackList, setTrackList] = useState(
+    MusicCategories.MusicCategories[0].tracks
+  );
+
+  useEffect(() => {});
+
+  const handleCategory = async (e) => {
+    const musicCategories = MusicCategories.MusicCategories;
+    const category = e.target.innerHTML;
+    setCategory(category);
+    const foundCategory = musicCategories.find(
+      (cat) => cat.category === category
+    );
+    setTrackList(foundCategory.tracks);
+  };
   return (
     <>
       <div className="homepage">
@@ -13,33 +32,39 @@ export default function Music() {
             <div className="categories-list-container-left">
               <ul className="categories-list">
                 <li className="music-category">
-                  <h3>Melodically Rich</h3>
+                  <h3 onClick={handleCategory}>Melodically Rich</h3>
                 </li>
                 <li className="music-category">
-                  <h3>Melodically Rich</h3>
+                  <h3 onClick={handleCategory}>Ambient</h3>
                 </li>
                 <li className="music-category">
-                  <h3>Melodically Rich</h3>
+                  <h3 onClick={handleCategory}>Dark/Noisey</h3>
                 </li>
               </ul>
             </div>
             <div className="track-list">
-              <ul className="categories-list">
-                <li className="track">track one</li>
-                <li className="track">track two</li>
-                <li className="track">track three</li>
-              </ul>
+              {trackList && (
+                <ul className="categories-list">
+                  {trackList.map((track) => {
+                    return (
+                      <li className="track" key={track.id}>
+                        {track.trackName}
+                      </li>
+                    );
+                  })}
+                </ul>
+              )}
             </div>
             <div className="categories-list-container-right">
               <ul className="categories-list">
                 <li className="music-category">
-                  <h3>Melodically Rich</h3>
+                  <h3 onClick={handleCategory}>Poppy</h3>
                 </li>
                 <li className="music-category">
-                  <h3>Melodically Rich</h3>
+                  <h3 onClick={handleCategory}>Alt-Beats</h3>
                 </li>
                 <li className="music-category">
-                  <h3>Melodically Rich</h3>
+                  <h3 onClick={handleCategory}>Piano</h3>
                 </li>
               </ul>
             </div>
---
+++ src/MusicTracks.js
@@ -0,0 +1,34 @@
+export default {
+  MusicCategories: [
+    {
+      category: 'Melodically Rich',
+      tracks: [
+        {
+          id: 1,
+          trackName: 'MR Track One',
+          fileName: 'Track_One.mp3',
+        },
+        {
+          id: 2,
+          trackName: 'MR Track Two',
+          fileName: 'Track_Two.mp3',
+        },
+      ],
+    },
+    {
+      category: 'Ambient',
+      tracks: [
+        {
+          id: 1,
+          trackName: 'Amb Track One',
+          fileName: 'Track_One.mp3',
+        },
+        {
+          id: 2,
+          trackName: 'Amb Track Two',
+          fileName: 'Track_Two.mp3',
+        },
+      ],
+    },
+  ],
+};
